24131b21c9c2f1e5fad8ac688ac849c99b48c0f6
Ruben <ralonzo@cyberwarrior.com>
2024-03-29 12:56:40 -0400

    Add basic k6 test

---
+++ SQA.LoadTest.Api/Scripts/basic-test.js
@@ -0,0 +1,7 @@
+﻿import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+    http.get('https://localhost:7292/string/reverse?input=AQS%C2%A0dlrow%C2%A0olleh');
+    sleep(1);
+}
